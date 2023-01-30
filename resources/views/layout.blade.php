1b5f47773ad23afad2b16fedb5d44fcb8a4c16dd
Stylesproline <stylesproline@gmail.com>
2023-01-30 20:28:35 +0300

    add new sections

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,315 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>МОНТАЖ И РЕМОНТ КРОВЛИ В МИНСКЕ И МИНСКОЙ ОБЛАСТИ ПОД КЛЮЧ</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="{{ url('assets/favicon.ico')}}" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{ url('css/styles.css')}}" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="tel:+37529XXXXXXX">+375-29-XXX-XX-XX</a><img src="./assets/img/log_vt.png"></a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#about">О нас</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Сервис</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Наши работы</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Контакт</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">МОНТАЖ И РЕМОНТ КРОВЛИ МИНСК И МИНСКАЯ ОБЛАСТЬ</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">Обшивка сайдингом под ключ.Большой опыт.Высокое качество.Гарантия!</p>
+                        <a class="btn btn-primary btn-xl" href="viber://chat?number=%2B37529XXXXXXX">расчитать стоимость</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Все виды кровелных работ под ключ!</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">Монтаж и демонтаж кровли из металочерепицы,<br>
+ондулина,шифера,профнастила,<br>
+гибкой кровельной черепицы </p>
+                        <a class="btn btn-light btn-xl" href="#services">Получить предложение</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Порядок нашей работы</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-gem fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">ЧТО СОГЛАСОВЫВАЕМ?</h3>
+                            <p class="text-muted mb-0">Что необходимо сделать? <br>Монтаж новой кровли.<br>Демонтаж.<br>Замена старой кровли или реконструкция.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-laptop fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">ВЫЕЗД СПЕЦИАЛИСТА ДЛЯ ЗАМЕРА</h3>
+                            <p class="text-muted mb-0">Для более детального расчета и согласования объемов работ наш специалист выезжает на объект,проводит все небходимые замеры и при небходимости дает косультации по используемым материалам и комплектующим.Предлагаем помощь в выборе и приобретении ,если необходимо, согласовываем цену,начало работы. </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-globe fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">НАЧИНАЕМ РАБОТУ</h3>
+                            <p class="text-muted mb-0">Согласовываем с вами удобное время начала работы на объекте(дата,сроки,оплата) и осуществляем их !</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-heart fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">СДАЧА ОБЪЕКТА</h3>
+                            <p class="text-muted mb-0">Завершение работ</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio-->
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/1.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/1.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">монтаж кровли</div>
+                                <div class="project-name">Битумная черепица</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/2.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/2.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">монтаж кровли</div>
+                                <div class="project-name">Металочерепица</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">монтаж кровли</div>
+                                <div class="project-name">Ондулин</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Кровля под ключ">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Кровля под ключ</div>
+                                <div class="project-name">Кровельная система</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">монтаж кровли</div>
+                                <div class="project-name">Металпрофиль</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg" title="кровля под ключ">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="..." />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">кровля под ключ</div>
+                                <div class="project-name">Металочерепица</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+          <!-- Portfolio-->
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/7.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/7.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">кровля под ключ</div>
+                                <div class="project-name">Металпрофиль</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/88.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/88.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">обшивка сайдингом</div>
+                                <div class="project-name">обшивка сайдингом</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/99.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/99.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">монтаж кровли</div>
+                                <div class="project-name">Металочерепица</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/55.jpg" title="Кровля под ключ">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/55.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Кровля под ключ</div>
+                                <div class="project-name">Кровельная система</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/77.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/77.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">обшивка сайдингом</div>
+                                <div class="project-name">обшивка сайдингом</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/45.jpg" title="кровля под ключ">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/45.jpg" alt="..." />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">кровля под ключ</div>
+                                <div class="project-name">Металпрофиль</div>
+                                </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/IMG-11.jpg" title="Кровля под ключ">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/IMG-11.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Кровля под ключ</div>
+                                <div class="project-name">Кровельная система</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/IMG-22.jpg" title="монтаж кровли">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/IMG-22.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">обшивка сайдингом</div>
+                                <div class="project-name">обшивка сайдингом</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/IMG-33.jpg" title="кровля под ключ">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/IMG-33.jpg" alt="..." />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">кровля под ключ</div>
+                                <div class="project-name">Металпрофиль</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Call to action-->
+        <section class="page-section bg-dark text-white">
+            <div class="container px-4 px-lg-5 text-center">
+                <h2 class="mb-4">НАШИ ОБЪЕКТЫ</h2>
+                <a class="btn btn-light btn-xl" href="tel:+37529XXXXXXX">ПОЛУЧИТЬ КОНСУЛЬТАЦИЮ!</a>
+                
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6 text-center">
+                        <h2 class="mt-0">Связаться с нами</h2>
+                        <hr class="divider" />
+                        <p class="text-muted mb-5">Свяжитесь с нами удобным для вас способом и мы  ответим на вопросы!</p>
+                        <div class="d-grid"><button class=" " id="submitButton" type="submit"></button><br>
+                        <a href="viber://chat?number=%2B37529XXXXXXX" class="btn btn-primary btn-xl-1" role="button">Viber</a><br>
+                        <a href="https://t.me/+37529XXXXXXX" class="btn btn-info btn-xl-2" role="button">Telegram</a><br>
+                        <a href="https://api.whatsapp.com/send?phone=37529XXXXXXX" class="btn btn-success btn-xl-3" role="button">WhatsApp</a><br>
+                     </div>
+                </div>
+                <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+                    <div class="col-lg-6">
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- * * SB Forms Contact Form * *-->
+                        <!-- * * * * * * * * * * * * * * *-->
+                        <!-- This form is pre-integrated with SB Forms.-->
+                        <!-- To make this form functional, sign up at-->
+                        <!-- https://startbootstrap.com/solution/contact-forms-->
+                        <!-- to get an API token!-->
+                       
+                    </div>
+                </div> 
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-4 text-center mb-5 mb-lg-0">
+                        <i class="bi-phone fs-2 mb-3 text-muted"></i>
+                        <div>+375-29-XXX-XX-XX</div>
+
+                      </div>
+                </div>
+            </div>
+        </section>
+        
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+        
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">ИП   УНП 49XXXXX &copy; 2022</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
